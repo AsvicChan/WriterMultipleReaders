833f8d95d17b54e8445989969144e578b330de19
Viktor <asvickun@gmail.com>
2018-07-16 15:19:26 -0700

    Made Writer for tests

--- WmR/WmR/Writer.h
+++ WmR/WmR/Writer.h
@@ -1,18 +1,34 @@
 #pragma once
+#include <iostream>
+#include <thread>
+#include "Controller.h"
+#include "Windows.h"
 
 class Writer
 {
 private:
+	
+	Controller& controller_;
+	void write()
+	{
+		for (int i = 0; i != 100; ++i)
+		{
+			controller_.changeValue(i);
+			std::cout << "Wrote " << i << ";"<<std::endl;
+			Sleep(1000);
+		}
+	};
+	std::thread thread_;
 
 public:
 
-	Writer()
+	Writer(Controller& controller) : controller_(controller), thread_(&Writer::write, this)
 	{
-
+		
 	};
 
 	~Writer()
 	{
-
+		thread_.join();
 	};
 };
