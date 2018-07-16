8b2171ab914716c2cbc2ae4169401b3cad85495f
Viktor <asvickun@gmail.com>
2018-07-16 15:20:43 -0700

    Made Reader for tests

--- WmR/WmR/Reader.h
+++ WmR/WmR/Reader.h
@@ -1,18 +1,34 @@
 #pragma once
+#include <iostream>
+#include "Controller.h"
+#include <thread>
+#include "Windows.h"
 
 class Reader
 {
 private:
+	int id_;
+	Controller& controller_;
+	void read()
+	{
+		for (int i = 0; i != 1000000; ++i)
+		{
+			int n = controller_.getValue();
+			std::cout << "Reader " << id_ << " read " << n << ";" << std::endl;
+			Sleep(10);
+		}
+	};
+	std::thread thread_;
 
 public:
 
-	Reader()
+	Reader(int id, Controller& controller) : controller_(controller), id_(id), thread_(&Reader::read, this)
 	{
 
 	};
 
 	~Reader()
 	{
-
+		thread_.join();
 	};
 };
