f9129fe5e8cde9051c8628017c645bd7e6e27723
Viktor <asvickun@gmail.com>
2018-07-16 15:18:45 -0700

    Controller must work now

--- WmR/WmR/Controller.h
+++ WmR/WmR/Controller.h
@@ -1,9 +1,14 @@
 #pragma once
+#include <condition_variable>
+#include <mutex>
 
 class Controller
 {
 private:
-
+	int value_;
+	std::mutex mutex_;
+	std::condition_variable cv_;
+	bool writing;
 public:
 
 	Controller()
@@ -14,5 +19,22 @@ public:
 	~Controller()
 	{
 
+	};
+
+	void changeValue(int newvalue)
+	{
+		std::lock_guard<std::mutex> lock(mutex_);
+		writing = true;
+		cv_.notify_all();
+		value_ = newvalue;
+		writing = false;
+		cv_.notify_all();
+	};
+
+	int getValue()
+	{
+		std::unique_lock<std::mutex> lck(mutex_);
+		cv_.wait(lck, [this]() {return (!writing); });
+		return value_;
 	};
 };
